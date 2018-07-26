3c11a843b0aaa83d05d5e684ce09992eaa07a149
Danny van Kooten <dannyvankooten@gmail.com>
2018-07-26 13:07:03 +0200

    fix golint warning in doc.go

--- doc.go
+++ doc.go
@@ -1,18 +1,16 @@
-/*
-Meddler is a small toolkit to take some of the tedium out of moving data
-back and forth between sql queries and structs.
+// Package Meddler is a small toolkit to take some of the tedium out of moving data
+// back and forth between SQL queries and structs.
+//
+// It is not a complete ORM. It is intended to be lightweight way to add some
+// of the convenience of an ORM while leaving more control in the hands of the
+// programmer.
+//
+// Package docs are available at:
+//
+// 	http://godoc.org/github.com/russross/meddler
+//
+// The package is housed on GitHub, and the README there has more info:
+//
+// 	http://github.com/russross/meddler
 
-It is not a complete ORM. It is intended to be lightweight way to add some
-of the convenience of an ORM while leaving more control in the hands of the
-programmer.
-
-Package docs are available at:
-
-	http://godoc.org/github.com/russross/meddler
-
-The package is housed on github, and the README there has more info:
-
-	http://github.com/russross/meddler
-
-*/
 package meddler
